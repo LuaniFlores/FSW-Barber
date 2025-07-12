fe707ce002af6c04d40d17e81a727fb014041f47
LuaniFlores <luanisales@gmail.com>
2025-07-12 17:45:02 -0300

    feat:  add Header  component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
-"use client";
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
+import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
-import { Button } from "./_components/ui/button";
-
-
-// SERVER COMPONENTS
 const Home = () => {
-  return <Button>oi</Button>
-};
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Luani</h2>
+        <p>Sabádo, 12 de Julho.</p>
 
-export default Home;
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça uma busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
